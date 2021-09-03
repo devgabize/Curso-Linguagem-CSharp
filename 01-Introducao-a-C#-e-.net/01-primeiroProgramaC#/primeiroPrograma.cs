2e1173730d16160c5c91614fa1d45f04f6a1cb85
gabrielpereiraa <gabrielpereira1970@gmail.com>
2021-09-02 22:00:59 -0300

    Primeiro programa em C#!

---
+++ 01-Introducao-a-C#-e-.net/01-primeiroProgramaC#/primeiroPrograma.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace _01_primeiroProgramaC_{
+    class primeiroPrograma{
+        static void Main(string[] args){
+            Console.WriteLine("OLÁ MUNDO! Meu primeiro código em C#! UHUUUUU");
+        }
+    }
+}
