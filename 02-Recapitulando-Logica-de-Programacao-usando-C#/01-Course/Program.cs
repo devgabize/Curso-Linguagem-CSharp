e60fb431ae3fa9142ee043a1cb6b96b40a35b10a
gabrielpereiraa <gabrielpereira1970@gmail.com>
2021-09-06 11:36:17 -0300

    Códigos criados em aula + organização arquivos!

--- 02-Recapitulando-Logica-de-Programacao-usando-C#/01-Course/Program.cs
+++ 02-Recapitulando-Logica-de-Programacao-usando-C#/01-Course/Program.cs
@@ -32,7 +32,7 @@ namespace _01_Course{
             System.Console.WriteLine(saldo.ToString("F4"));
             System.Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture));*/
 
-            int idade = 32;
+            /*int idade = 32;
             double saldo = 10.35784;
             string nome = "Maria";
 
@@ -40,7 +40,49 @@ namespace _01_Course{
 
             System.Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
 
-            System.Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais ");
+            System.Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais ");*/
+
+            /*string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+
+            //string s = Console.ReadLine();
+
+            string[] v = Console.ReadLine().Split(' ');
+            string a = v[0];
+            string b = v[1];
+            string c = v[2];
+
+            System.Console.WriteLine("\nVocê digitou: ");
+            System.Console.WriteLine(frase);
+            System.Console.WriteLine(x);
+            System.Console.WriteLine(y);
+            System.Console.WriteLine(z);
+            System.Console.WriteLine(a);
+            System.Console.WriteLine(b);
+            System.Console.WriteLine(c);*/
+
+            int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            string[] vet = Console.ReadLine().Split(' ');
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
+
+            System.Console.WriteLine("\nVocê digitou: ");
+            System.Console.WriteLine(n1);
+            System.Console.WriteLine(ch);
+            System.Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture));
+            System.Console.WriteLine(nome);
+            System.Console.WriteLine(sexo);
+            System.Console.WriteLine(idade);
+            System.Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+
         }
     }
 }
---
+++ 02-Recapitulando-Logica-de-Programacao-usando-C#/03-ExercicioFixacao/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace _03_ExercicioFixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Entre com seu nome completo: ");
+            string nomeCompleto = Console.ReadLine();
+
+            Console.WriteLine("Quantos quartoos tem na sua casa? ");
+            int numQuartos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o preço de um produto: ");
+            float preco = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com seu último nome, idade e altura (mesma linha): ");
+            string[] infos = Console.ReadLine().Split(' ');
+            string sobrenome = infos[0];
+            int idade = int.Parse(infos[1]);
+            float altura = float.Parse(infos[2], CultureInfo.InvariantCulture);
+
+            System.Console.WriteLine("\n" + nomeCompleto);
+            System.Console.WriteLine(numQuartos);
+            System.Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+            System.Console.WriteLine(sobrenome);
+            System.Console.WriteLine(idade);
+            System.Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+            
+        }
+    }
+}
